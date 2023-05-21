f1f15b5bfaddb5fa6470f765adf5b8e6135f04b9
Gabriel Napolis <gabrielnapolis1604@gmail.com>
2023-05-21 10:55:19 -0300

    out system

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ function operation() {
 
       if (action === "Criar Conta") {
         createAccount();
+      } else if(action === "Depositar"){
+
+      } else if(action === "Consultar Saldo"){
+        
+      } else if(action === "Sacar"){
+
+      } else if(action === "Sair"){
+        console.log(chalk.bgBlue.black('Obrigado por usar nosso banco!'))
+        process.exit()
       }
     })
     .catch((err) => console.log(err));
